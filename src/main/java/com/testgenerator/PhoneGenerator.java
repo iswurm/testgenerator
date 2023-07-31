f2977b72aca9de4dc5d7c90ae151371c5f30b84c
Ignacio Sánchez <ignacio.sanchez@sisnet360.com>
2023-07-31 09:38:43 +0200

    Generador de numeros de telefono

---
+++ src/main/java/com/testgenerator/PhoneGenerator.java
@@ -0,0 +1,31 @@
+package com.testgenerator;
+
+import java.util.Random;
+
+public class PhoneGenerator {
+
+    // Método que genera un número de teléfono móvil de España aleatorio
+    public static String generarNumeroTelefonoMovil() {
+        Random random = new Random();
+
+        // Código de país para España
+        int codigoPais = 34;
+
+        // Código de operador (primer dígito es 6 o 7)
+        int codigoOperador = 6 + random.nextInt(2); // Genera 6 o 7
+
+        // Generar los 7 dígitos restantes del número local
+        int numeroLocal = random.nextInt(9000000) + 1000000;
+
+        // Formatear el número de teléfono
+        return String.format("%d%d", codigoOperador, numeroLocal);
+    }
+
+    public static void main(String[] args) {
+        // Ejemplo de uso
+        for (int i = 0; i < 5; i++) {
+            String numeroTelefonoAleatorio = generarNumeroTelefonoMovil();
+            System.out.println("Número de teléfono móvil de España aleatorio " + (i + 1) + ": " + numeroTelefonoAleatorio);
+        }
+    }
+}
