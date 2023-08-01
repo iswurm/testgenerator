4564f3ff75a2810e9c1828f142f84bc87cad0756
Ignacio Sánchez <ignacio.sanchez@sisnet360.com>
2023-08-01 11:11:54 +0200

    [FEATURE] Mejora de DateGenerator y TestGatos

--- src/main/java/com/testgenerator/DateGenerator.java
+++ src/main/java/com/testgenerator/DateGenerator.java
@@ -9,14 +9,15 @@ public class DateGenerator {
     public static String generateDate() {
         Random random = new Random();
         int maxEdad = 90;
-        int edad = random.nextInt(maxEdad + 1);
+        int minEdad = 18;
+        int edad = minEdad + random.nextInt(maxEdad + 1);
 
         Calendar calendar = GregorianCalendar.getInstance();
         calendar.add(Calendar.YEAR, -edad);
 
         int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH) + 1; // Los meses en Calendar se indexan desde 0 (enero) a 11 (diciembre)
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = random.nextInt(12) + 1; // Los meses van de 1 a 12 (enero a diciembre)
+        int day = random.nextInt(getMaxDayOfMonth(year, month)) + 1; // Día aleatorio del mes
 
         String fechaNacimiento = String.format("%02d/%02d/%04d", day, month, year);
         return fechaNacimiento;
@@ -31,10 +32,15 @@ public class DateGenerator {
         calendar.add(Calendar.YEAR, -edad);
 
         int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH) + 1; // Los meses en Calendar se indexan desde 0 (enero) a 11 (diciembre)
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = random.nextInt(12) + 1; // Los meses van de 1 a 12 (enero a diciembre)
+        int day = random.nextInt(getMaxDayOfMonth(year, month)) + 1; // Día aleatorio del mes
 
         String fechaNacimiento = String.format("%02d/%02d/%04d", day, month, year);
         return fechaNacimiento;
+    }
+
+    public static int getMaxDayOfMonth(int year, int month) {
+        Calendar calendar = new GregorianCalendar(year, month - 1, 1);
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 }
